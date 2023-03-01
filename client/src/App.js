9a928c3b3d2c24fdce90a3e7376d1bfc42ba43e3
Ben Fifield <actuallysmart@yahoo.com>
2023-02-28 21:14:03 -0800

    added authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from 'react';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import List from './pages/List';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -18,7 +46,18 @@ function App() {
               path="/" 
               element={<Home />} 
             />
-
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route
+                path="/list"
+                element={<List />}
+              />
           </Routes>
         </div>
       </Router>
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,10 +1,81 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
+
 const resolvers = {
-    Query: {
-       
+  Query: {
+    users: async () => {
+      return User.find().populate('thoughts');
     },
-    Mutation: {
-
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('thoughts');
     },
-}
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { thoughtId }) => {
+      return Thought.findOne({ _id: thoughtId });
+    },
+  },
 
-module.exports = resolvers
+  Mutation: {
+    addUser: async (parent, args) => {
+        console.log(args);
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addThought: async (parent, { thoughtText, thoughtAuthor }) => {
+      const thought = await Thought.create({ thoughtText, thoughtAuthor });
+
+      await User.findOneAndUpdate(
+        { username: thoughtAuthor },
+        { $addToSet: { thoughts: thought._id } }
+      );
+
+      return thought;
+    },
+    addComment: async (parent, { thoughtId, commentText, commentAuthor }) => {
+      return Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        {
+          $addToSet: { comments: { commentText, commentAuthor } },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
+    removeThought: async (parent, { thoughtId }) => {
+      return Thought.findOneAndDelete({ _id: thoughtId });
+    },
+    removeComment: async (parent, { thoughtId, commentId }) => {
+      return Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        { $pull: { comments: { _id: commentId } } },
+        { new: true }
+      );
+    },
+  },
+};
+
+module.exports = resolvers;
